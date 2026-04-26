41a4d11c458d9f37564ec8d204837f358dac5518
pushpakkolya <kolyapushpak70@gmail.com>
2026-04-26 19:22:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const targetDate = new Date("July 5, 2026 11:00:00").getTime();
 
+function updateCountdown() {
+  const now = new Date().getTime();
+  const gap = targetDate - now;
+
+  const days = Math.floor(gap / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((gap / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((gap / (1000 * 60)) % 60);
+  const seconds = Math.floor((gap / 1000) % 60);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}
+
+setInterval(updateCountdown, 1000);
